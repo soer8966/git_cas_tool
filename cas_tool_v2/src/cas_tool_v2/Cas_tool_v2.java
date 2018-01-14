db0062ed0709216627762a995b03160be5786ae2
Soren Witte <gedergodt@gmail.com>
2018-01-14 14:19:13 +0100

    fourth pc

--- cas_tool_v2/src/cas_tool_v2/Cas_tool_v2.java
+++ cas_tool_v2/src/cas_tool_v2/Cas_tool_v2.java
@@ -15,7 +15,7 @@ public class Cas_tool_v2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("testing 3, from mac");
+        System.out.println("testing 3, from mac and pc");
     }
     
 }
