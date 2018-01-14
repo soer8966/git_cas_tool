08311a08bfd8e295a8c21a5b4198d716a73824eb
sorenwitte <sorenwitte@gmail.com>
2018-01-14 13:52:59 +0100

    first

---
+++ src/castry/Castry.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package castry;
+
+import java.util.ArrayList;
+
+public class Castry {
+    
+    public static void main(String[] args) {
+        System.out.println("Testing git");
+    }
+    
+}
