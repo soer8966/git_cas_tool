adcb09d546b3cb34abed41c4c5f40c478a95965b
sorenwitte <sorenwitte@gmail.com>
2018-01-14 14:16:29 +0100

    third from mac

--- cas_tool_v2/src/cas_tool_v2/Cas_tool_v2.java
+++ cas_tool_v2/src/cas_tool_v2/Cas_tool_v2.java
@@ -15,7 +15,7 @@ public class Cas_tool_v2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("testing 3");
+        System.out.println("testing 3, from mac");
     }
     
 }
